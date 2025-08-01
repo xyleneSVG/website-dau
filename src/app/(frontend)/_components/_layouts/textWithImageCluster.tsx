52f65e3940c6807f7a938407367a7f9bee630332
Abdulrozak <muhammaddhani494@gmail.com>
2025-08-01 13:56:51 +0700

    refactor: resize image height

--- src/app/(frontend)/_components/_layouts/textWithImageCluster.tsx
+++ src/app/(frontend)/_components/_layouts/textWithImageCluster.tsx
@@ -69,7 +69,7 @@ export default function TextWithImageCluster({ data, domainBlob, getLucideIcon }
             {imageLists?.slice(1).map((img, idx) => (
               <div
                 key={idx}
-                className="w-[48%] h-[100px] sm:h-[150px] lg:h-[160px] xl:h-[180px] 2xl:h-[200px] aspect-[3/2] relative rounded-2xl overflow-hidden"
+                className="w-[48%] h-[70px] sm:h-[100px] lg:h-[120px] xl:h-[150px] 2xl:h-[180px] aspect-[3/2] relative rounded-2xl overflow-hidden"
               >
                 <Image
                   src={domainBlob + img.itemImage?.filename}
