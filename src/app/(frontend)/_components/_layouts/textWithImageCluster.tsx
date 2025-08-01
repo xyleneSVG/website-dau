8de4c5db952bc7e272ba36fde29ce52a6dde9d42
Abdulrozak <muhammaddhani494@gmail.com>
2025-08-01 13:28:28 +0700

    refactor: update image pn TextWithImageCluster

--- src/app/(frontend)/_components/_layouts/textWithImageCluster.tsx
+++ src/app/(frontend)/_components/_layouts/textWithImageCluster.tsx
@@ -54,13 +54,14 @@ export default function TextWithImageCluster({ data, domainBlob, getLucideIcon }
       >
         <div className="w-full lg:w-1/2 flex flex-col items-center gap-4 md:gap-6 mb-0">
           {imageLists?.length > 0 && (
-            <div className="w-[70%] sm:w-[50%] lg:w-full relative aspect-[4/3] rounded-2xl lg:rounded-4xl overflow-hidden">
+            <div className="w-[70%] sm:w-[50%] lg:w-full rounded-2xl lg:rounded-4xl overflow-hidden">
               <Image
                 src={domainBlob + imageLists[0].itemImage?.filename}
                 alt="Image"
-                fill
+                width={800} 
+                height={0} 
                 priority
-                className="object-cover"
+                className="w-full h-auto object-cover"
               />
             </div>
           )}
